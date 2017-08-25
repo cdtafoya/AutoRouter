08a7bd08fbf9d0494e1ddc017525738c4cf925f0
cdtafoya <cdtafoya@miners.utep.edu>
2017-08-25 14:23:58 -0600

    Contains only Mikami Tabuchi in AutoRouter.py

---
+++ point.py
@@ -0,0 +1,17 @@
+'''
+Created on Jul 31, 2017
+
+@author: Carlos
+'''
+
+class point:
+    '''
+    classdocs
+    '''
+
+    def __init__(self, x, y):
+        '''
+        Constructor
+        '''
+        self.X = x
+        self.Y = y
